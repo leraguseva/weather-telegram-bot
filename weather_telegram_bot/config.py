139d9e4b089c7a43ec687f0aa1aa3ff8f55fad4f
leraguseva <144918965+leraguseva@users.noreply.github.com>
2024-11-07 22:20:38 +0200

    Update config.py

--- weather_telegram_bot/config.py
+++ weather_telegram_bot/config.py
@@ -1,2 +1,2 @@
-open_weather_token = "3e6ca1fc2cbecdf58768624b9eef373b"
-tg_bot_token = "6517540356:AAGAu2giDF1zfk4DZbbUqcrtFJO7uMyTpbM"
+open_weather_token = "open_weather_api"
+tg_bot_token = "your_bot_tokem"
